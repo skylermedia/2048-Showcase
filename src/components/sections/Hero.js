9bbcf21abbbd7e0c568a011fdaf11e293c0d9d8f
healthyteen <118077323+healthyteen@users.noreply.github.com>
2023-01-12 08:56:06 -0800

    Changed Header Name

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -61,7 +61,7 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              <span className="text-color-primary">2048</span>
+              <span className="text-color-primary">3096 – Blocks</span>
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
