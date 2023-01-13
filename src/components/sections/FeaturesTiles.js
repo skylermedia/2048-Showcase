8070e25f172b87ce04b0eceeeefc7ceeebc257cf
healthyteen <118077323+healthyteen@users.noreply.github.com>
2023-01-13 08:58:51 -0800

    Updated FeatureTiles.js

--- src/components/sections/FeaturesTiles.js
+++ src/components/sections/FeaturesTiles.js
@@ -43,11 +43,6 @@ const FeaturesTiles = ({
     pushLeft && 'push-left'
   );
 
-  const sectionHeader = {
-    title: 'Build up the whole picture',
-    paragraph: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum — semper quis lectus nulla at volutpat diam ut venenatis.'
-  };
-
   return (
     <section
       {...props}
